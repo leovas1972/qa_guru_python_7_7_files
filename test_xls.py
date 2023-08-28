c893e27040cf8079250d90c11cd01faf4c947697
Vasilii Leontev <leovas1972@gmail.com>
2023-08-28 14:05:16 +0700

    running a test on a test_xls

--- test_xls.py
+++ test_xls.py
@@ -1,14 +1,23 @@
-import xlrd
+import xlrd, os
+
+from conftest import JOINED_PATH
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-def test_xls()
-    book = xlrd.open_workbook('resources/file_example_XLS_10.xls')
-
+def test_xls():
+    book = xlrd.open_workbook(os.path.join(JOINED_PATH, 'file_example_XLS_10.xls'))
+    print(f'Количество листов {book.nsheets}')
     print(f'Количество листов {book.nsheets}')
     print(f'Имена листов {book.sheet_names()}')
     sheet = book.sheet_by_index(0)
     print(f'Количество колонок  {sheet.ncols}')
     print(f'Количество строк    {sheet.nrows}')
     print(f'Пересечение строки и столбца {sheet.cell_value(rowx=3, colx=2)}')
+    result = []
     for rx in range(sheet.nrows):
-        print(sheet.row(rx))
+        result.append(sheet.row(rx))
+        print(result)
+    assert book.nsheets == 1
+    assert book.sheet_names() == ['Sheet1']
+    assert sheet.ncols == 8
+    assert sheet.cell_value(rowx=3, colx=2) == 'Gent'
+    assert  len(result) == 10
