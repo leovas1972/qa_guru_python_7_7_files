89cd3cfb6412ce0d4a246c0ff4550d6f8818876b
Vasilii Leontev <leovas1972@gmail.com>
2023-08-28 12:45:23 +0700

    running a test on a test_pdf

--- test_pdf.py
+++ test_pdf.py
@@ -1,15 +1,21 @@
-import pypdf
+import pypdf, os
+from conftest import JOINED_PATH
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-reader = pypdf.PdfReader('resources/docs-pytest-org-en-latest.pdf')
-number_of_pages = len(reader.pages)
-first_page = reader.pages[0]
-text = first_page.extract_text()
-# print(number_of_pages)
-# print(first_page)
-print(text)
-count = 0
-for image_file in first_page.images:
-    with open(str(count) + image_file.name, 'wb') as fp:
-        fp.write(image_file.data)
-        count += 1
+def test_pdf():
+    reader = pypdf.PdfReader(os.path.join(JOINED_PATH, 'docs-pytest-org-en-latest.pdf'))
+    number_of_pages = len(reader.pages)
+    first_page = reader.pages[0]
+    text = first_page.extract_text()
+    print(text)
+    print(number_of_pages)
+    print(first_page)
+    print(text)
+    count = 0
+    for image_file in first_page.images:
+         with open(os.path.join(JOINED_PATH, str(count) + image_file.name), 'wb') as fp:
+            fp.write(image_file.data)
+            count += 1
 
+    assert number_of_pages == 412
+    assert text == "pytest Documentation\nRelease 0.1\nholger krekel, trainer and consultant, https://merlinux.eu/\nJul 14, 2022"
+    assert count == 1
