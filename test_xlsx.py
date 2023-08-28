4a35a789a9c60c232f9ab95cdd5bc7541e096692
Vasilii Leontev <leovas1972@gmail.com>
2023-08-28 14:12:06 +0700

    added path in workbook

--- test_xlsx.py
+++ test_xlsx.py
@@ -3,6 +3,6 @@ import os
 from conftest import JOINED_PATH
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
+workbook = load_workbook(os.path.join(JOINED_PATH, 'file_example_XLSX_50.xlsx'))
 sheet = workbook.active
 print(sheet.cell(row=3, column=2).value)
